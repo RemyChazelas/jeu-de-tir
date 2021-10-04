f0ca60c51b62998d53c43c32888c0f7d655622df
Remy Chazelas <remychazelas@gmail.com>
2021-10-04 17:13:40 +0200

    add some html & css & class for weapon & huge weapon

--- index.js
+++ index.js
@@ -35,3 +35,57 @@ class Player {
         context.fill();
     }
 }
+
+// Creating Weapon Class
+
+class Weapon {
+    constructor(x, y, radius, color, velocity, damage) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.damage = damage;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(
+            this.x,
+            this.y,
+            this.radius,
+            (Math.PI / 180) * 0,
+            (Math.PI / 180) * 360,
+            false
+        );
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
+
+// Creating HugeWeapon Class
+
+class HugeWeapon {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.color = "rgba(255,0,133,1)";
+    }
+
+    draw() {
+        context.beginPath();
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, 200, canvas.height);
+    }
+
+    update() {
+        this.draw();
+        this.x += 20;
+    }
+}
